c4285d46ea4c429acd0790c34dd92f91569ace25
Bruno Beltran <brunobeltran0@gmail.com>
2018-11-13 07:26:30 -0800

    BUGFIX: stats loads now, needed vvcf_table from Tom

--- multi_locus_analysis/stats.py
+++ multi_locus_analysis/stats.py
@@ -3,8 +3,10 @@ import pandas as pd
 import scipy
 from scipy.io import loadmat
 from scipy.signal import savgol_filter, savgol_coeffs
+from functools import lru_cache
 
 import gc
+from pathlib import Path
 
 ###############{{{
 # Call on DataFrame directly
@@ -245,6 +247,23 @@ def vc(t, delta, beta):
 def vcp(t, delta, beta):
     return ( np.power(np.abs(t + delta), beta)
            - np.power(np.abs(t), beta) )*np.power(delta, beta)
+
+deltas_ = np.linspace(-3, 3, 25) # placeholders for corresponding values in logspace
+alphas_ = np.linspace(0.25, 1, 31) # steps of 0.025
+tOverDeltas_ = np.linspace(0, 5, 501) # steps of 0.01
+vvcf_table_ = np.reshape(np.loadtxt(Path(__file__).parent / Path('vvcf_table.csv'),
+        delimiter=','), (31, 25, 501))
+def calc_vel_corr_fixed_(tOverDelta, deltaOverTDeltaN, alpha):
+    # this performs interpolation in logspace for "delta"/deltaOverTDeltaN
+    deltaOverTDeltaN = np.log10(deltaOverTDeltaN)
+    return scipy.interpolate.interpn((alphas_, deltas_, tOverDeltas_),
+                                     vvcf_table_,
+                                     (alpha, deltaOverTDeltaN, tOverDelta))
+calc_vel_corr_fixed_.vvcf = None
+calc_vel_corr_fixed = np.vectorize(calc_vel_corr_fixed_)
+
+def vvc_rescaled_theory(t, delta, beta, A, tDeltaN):
+    return 2*A*np.power(delta, beta)*(vc(t*delta, delta, beta) - calc_vel_corr_fixed(t, delta/tDeltaN, 2*beta))
 
 def haar_coeffs_w(n):
     return np.sign(np.arange(-n, n+1))/n/(n+1)
